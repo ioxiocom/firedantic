ac7f9f8c427ce545d3aa027830e997cc0f6df6e7
Joakim Nordling <joakim.nordling@ioxio.com>
2023-09-27 10:58:57 +0300

    Improve logging

--- firedantic/_sync/utils.py
+++ firedantic/_sync/utils.py
@@ -37,22 +37,25 @@ def set_up_ttl_policies(
         )
         field_obj = client.get_field({"name": path})
 
-        logger.debug(
-            'TTL config for "%s", collection: %s, field: "%s", state: %s',
+        # Variables for logging
+        readable_state = str(field_obj.ttl_config.state).removeprefix("State.")
+        log_str = '"%s", collection: "%s", field: "%s", state: "%s"'
+        log_params = [
             model.__name__,
             model.get_collection_name(),
             model.__ttl_field__,
-            str(field_obj.ttl_config.state).removeprefix("State."),
-        )
+            readable_state,
+        ]
 
         if field_obj.ttl_config.state == Field.TtlConfig.State.STATE_UNSPECIFIED:
-            logger.info(
-                'Setting up TTL config for "%s", collection: %s, field: "%s"',
-                model.__name__,
-                model.get_collection_name(),
-                model.__ttl_field__,
-            )
+            logger.info("Setting up new TTL config: " + log_str, *log_params)
             field_obj.ttl_config = Field.TtlConfig(
                 {"state": Field.TtlConfig.State.CREATING}
             )
             client.update_field({"field": field_obj})
+        elif field_obj.ttl_config.state == Field.TtlConfig.State.CREATING:
+            logger.info("TTL config is still being created: " + log_str, *log_params)
+        elif field_obj.ttl_config.state == Field.TtlConfig.State.NEEDS_REPAIR:
+            logger.error("TTL config needs repair: " + log_str, *log_params)
+        elif field_obj.ttl_config.state == Field.TtlConfig.State.ACTIVE:
+            logger.debug("TTL config is active: " + log_str, *log_params)
